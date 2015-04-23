51eabadac4106b381f50aa9aa3bb1562ed5b2bd3
Brian Finck <brian@brianfinck.com>
2015-04-23 15:50:21 -0700

    adding server

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/*', function(req, res) {
+  res.status(404).send('could not find page');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
