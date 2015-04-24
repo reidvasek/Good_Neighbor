0f4b29fa03c6f4ca39bc1af3063ea715bcd835e5
Eric Timmons <timmoeri000@yahoo.com>
2015-04-23 18:40:11 -0700

    add profiles, locations, emails

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -48,11 +48,16 @@ $(function() {
   };
 
   window.addEventListener('load', function(event) {
-    var post1 = new Post('DogavanMcnap', 'Eggs', 'I have 10 eggs I just don\'t need', 'http://aka.weightwatchers.com/images/1033/dynamic/GCMSImages/Egg_000001615195_main.jpg'),
-          post2 = new Post('John', 'Wrench', 'I have a wrench I don\'t need if anyone wants it?', 'http://aka.weightwatchers.com/images/1033/dynamic/GCMSImages/Egg_000001615195_main.jpg'),
-          post3 = new Post('Fred', 'Bananas', 'I bought these bananas for a smoothie but I only needed two! I\'ve got 3 left!', 'http://aka.weightwatchers.com/images/1033/dynamic/GCMSImages/Egg_000001615195_main.jpg');
+    var post1 = new Post('DogavanMcnap', 'Eggs', 'I have 10 eggs I just don\'t need', 'http://aka.weightwatchers.com/images/1033/dynamic/GCMSImages/Egg_000001615195_main.jpg', 'Seattle, WA', 'Dognap@example.com'),
+          post2 = new Post('John', 'Wrench', 'I have a wrench I don\'t need if anyone wants it?', 'http://www.blogcdn.com/www.diylife.com/media/2010/03/combination-wrench-590rr033010.jpg', 'San Fransisco, CA', 'John@example.com'),
+          post3 = new Post('Fred', 'Bananas', 'I bought these bananas for a smoothie but I only needed two! I\'ve got 3 left!', 'http://askgeorgie.com/wp-content/uploads/2012/11/banana.jpg', 'Portland, OR', 'Fred@example.com'),
+          post4 = new Post('Kevin Bacon', 'Sugar', 'I just have too much sugar for my own good.', 'http://www.beautybulletin.com/images/sugar_8560.jpg', 'Sand Diego, CA', 'KevinBacon@example.com'),
+          post5 = new Post('Miranda Karp', 'Firewood', 'Chopped all this up over the weekend, pretty sure I wont use it all any time soon.', 'http://mulchcapecod.com/images/seasoned-firewood.jpg', 'New York, NY', 'MirandaK@example.com'),
+          post6 = new Post('Trent Miles', 'Cup Noodles', 'Costco had a noodle sale, I couldn\'t resist.', 'http://goo.gl/SnTNBe', 'Golta, CA', 'TrentMiles@example.com'),
+          post7 = new Post('Chave Dapelle', 'Bottled Water', 'You can never have too much bottled water!', 'http://www.herald.co.zw/wp-content/uploads/2014/06/tapped-out-water-bottles_16601_600x450.jpg','Santa Clara, CA','ChaveDapelle@example.com'),
+          post8 = new Post('Eve Sterkel', 'Napkins', 'So Mcdonalds gave me a ton of napkins, I wont be needing them all.', 'http://www.rantlifestyle.com/wp-content/uploads/2014/06/703.-Napkins.jpg', 'Chicago, IL', 'EveSterkel@example.com');
 
-    premadeList.push(post1, post2, post3);
+    premadeList.push(post1, post2, post3, post4, post5, post6, post7, post8);
 
     var renderPreMade = function() {
       premadeList.forEach(function(list) {
